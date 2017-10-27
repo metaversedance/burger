65c6cd4dcdf8decd4b61c549b332a3d75dbe547a
Matthew Hoe <matthewh913@gmail.com>
2017-10-27 15:33:26 -0700

    update schema for default timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE `burgers` (
   `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
   `burger_name` VARCHAR( 255) NOT NULL,
   `devoured` BOOLEAN NOT NULL,
-  `date` DATETIME NOT NULL,
+  `date` DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   PRIMARY KEY ( `id` ) );
