ecaa9ad01fb14bf5911395bc98dd04a131e20ec4
Matthew Hoe <matthewh913@gmail.com>
2017-10-27 15:03:22 -0700

    burger

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql")
+
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name,devoured) 
+VALUES ("Krabby Patty",FALSE);
+
+
+INSERT INTO burgers (burger_name,devoured) 
+VALUES ("King Kong",FALSE);
+
+
+
+INSERT INTO burgers (burger_name,devoured) 
+VALUES ("Godzilla",FALSE);
